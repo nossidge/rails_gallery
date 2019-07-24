9dfa58b48489420941e23b5767ddf0ea973a21db
Paul Thompson <nossidge@gmail.com>
2019-07-24 17:44:05 +0100

    cleaner logic for sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,18 +5,32 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect_to home_url, notice: "Welcome back #{user.username}!"
-    else
-      flash.now[:alert] = 'Username or password is invalid'
-      render 'new'
-    end
+    return session_invalid unless session_params_present(params)
+
+    email = params[:email].downcase
+    user = User.find_by_email(email)
+
+    return session_invalid unless user&.authenticate(params[:password])
+
+    session[:user_id] = user.id
+    redirect_to home_url, notice: "Welcome back #{user.username}!"
   end
 
   def destroy
     session[:user_id] = nil
     redirect_to home_url, notice: 'Logged out! See you later :-)'
+  end
+
+  private
+
+  def session_params_present(params)
+    [:email, :password].all? do |param|
+      params.has_key?(param)
+    end
+  end
+
+  def session_invalid
+    flash.now[:alert] = 'Email or password is invalid'
+    render 'new'
   end
 end
