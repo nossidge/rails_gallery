bc70c2de197e5cf95b8ad82aaa0275d87f04ed6c
Paul Thompson <nossidge@gmail.com>
2019-07-24 14:09:50 +0100

    implement image thumbnails

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  ##
+  # Get the URL for a thumbnail variant of an {ActiveStorage} image.
+  # Use `#processed` to memoise the thumbnail generation.
+  #
+  # @param [ActiveStorage::Attached::One] image_attachment
+  #   The ActiveStorage image file
+  # @return [String]
+  #   The URL for the thumbnail variant
+  #
+  def url_for_thumbnail(image_attachment)
+    url_for image_attachment.variant(resize: '225x225').processed
+  end
 end
