9de527546a38ea6d95eb143367af422ff2ba8218
Paul Thompson <nossidge@gmail.com>
2019-07-30 22:57:02 +0100

    spec cleanup

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,3 +1,12 @@
+# frozen_string_literal: true
+
+# Necessary to add 'fixture_file_upload' method
+# https://blog.eq8.eu/til/factory-bot-trait-for-active-storange-has_attached.html
+FactoryBot::SyntaxRunner.class_eval do
+  include ActionDispatch::TestProcess
+end
+
+
 FactoryBot.define do
 
   factory :user do
@@ -23,6 +32,10 @@ FactoryBot.define do
 
   factory :image do
     gallery
+    file do
+      filepath = Rails.root.join('spec', 'support', 'assets', 'test_image.png')
+      fixture_file_upload filepath, 'image/png'
+    end
   end
 
 end
