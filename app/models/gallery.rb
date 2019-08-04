d3f15ffbcfe2260a509403340c8ce54ee73b3344
Paul Thompson <nossidge@gmail.com>
2019-08-04 12:20:25 +0100

    fix issues caught by testing

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,6 +1,6 @@
 class Gallery < ApplicationRecord
   belongs_to :user
-  has_many :images
+  has_many :images, dependent: :destroy
 
   validates :name, presence: true, length: { minimum: 3, maximum: 50 }
   validates :description, length: { maximum: 255 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :galleries
+  has_many :galleries, dependent: :destroy
 
   before_save { self.email = email.downcase }
 
